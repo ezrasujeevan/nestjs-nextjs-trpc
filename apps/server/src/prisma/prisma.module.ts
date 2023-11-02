5a0a99b3254ad064639e2bc48d687a35208b87b2
ezra <ezra.sujeevan@gmail.com>
2023-11-02 14:31:50 +0530

    completed backend services

--- apps/server/src/prisma/prisma.module.ts
+++ apps/server/src/prisma/prisma.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import {TasksModule} from "@server/tasks/tasks.module";
 
 @Module({
   providers: [PrismaService],
-  exports:[PrismaService]
+  exports: [PrismaService],
 })
 export class PrismaModule {}
--- apps/server/src/tasks/tasks.service.ts
+++ apps/server/src/tasks/tasks.service.ts
@@ -1,43 +1,40 @@
-import {Injectable} from '@nestjs/common';
-import {CreateTaskDto} from './dto/create-task.dto';
-import {UpdateTaskDto} from './dto/update-task.dto';
-import {PrismaService} from "@server/prisma/prisma.service";
+import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { PrismaService } from '@server/prisma/prisma.service';
 
 @Injectable()
 export class TasksService {
+  constructor(private prisma: PrismaService) {}
 
-    constructor(private prisma: PrismaService) {
-    }
+  async create(createTaskDto: CreateTaskDto) {
+    return await this.prisma.task.create({ data: createTaskDto });
+  }
 
-    async create(createTaskDto: CreateTaskDto) {
-        return await this.prisma.task.create({data: createTaskDto})
-    }
+  async findAll() {
+    return await this.prisma.task.findMany();
+  }
 
-    async findAll() {
-        return await this.prisma.task.findMany();
-    }
+  async findCompleted() {
+    return await this.prisma.task.findMany({ where: { status: true } });
+  }
 
-    async findCompleted() {
-        return await this.prisma.task.findMany({where: {status: true}})
-    }
+  async findIncomplete() {
+    return await this.prisma.task.findMany({ where: { status: false } });
+  }
 
-    async findIncomplete() {
-        return await this.prisma.task.findMany({where: {status: false}})
-    }
+  async findOne(id: number) {
+    return await this.prisma.task.findUnique({ where: { id } });
+  }
 
+  async update(id: number, updateTaskDto: UpdateTaskDto) {
+    return await this.prisma.task.update({
+      where: { id },
+      data: updateTaskDto,
+    });
+  }
 
-    async findOne(id: number) {
-        return await this.prisma.task.findUnique({where: {id}})
-    }
-
-    async update(id: number, updateTaskDto: UpdateTaskDto) {
-        return await this.prisma.task.update({
-            where: {id},
-            data: UpdateTaskDto,
-        })
-    }
-
-    async remove(id: number) {
-        return await this.prisma.task.delete({where: {id}})
-    }
+  async remove(id: number) {
+    return await this.prisma.task.delete({ where: { id } });
+  }
 }
