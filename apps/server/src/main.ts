c55363b7b94522e456f31992cf06333cf296bb77
ezra <ezra.sujeevan@gmail.com>
2023-11-02 12:00:15 +0530

    added swagger documentation

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TrpcRouter } from '@server/trpc/trpc.router';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const trpc = app.get(TrpcRouter);
   trpc.applyMiddleware(app);
+
+  const config = new DocumentBuilder()
+      .setTitle('Task')
+      .setDescription('The Task API description')
+      .setVersion('1')
+      .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
