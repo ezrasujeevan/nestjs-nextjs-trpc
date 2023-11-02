378222fd74fa19e32c08d6c417cb1e4fab9370b9
ezra <ezra.sujeevan@gmail.com>
2023-11-02 12:03:27 +0530

    added task crud operations

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TrpcModule } from '@server/trpc/trpc.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TrpcModule, PrismaModule],
+  imports: [ConfigModule.forRoot(), TrpcModule, PrismaModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
